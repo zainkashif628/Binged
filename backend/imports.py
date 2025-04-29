cfcd21f6ae0b40d3364fe6da785d292f4e91f7f1
Zain Kashif <zkashif.bscs23seecs@seecs.edu.pk>
2025-04-29 22:44:22 +0500

    implemented batching and removed picture uploads

--- backend/imports.py
+++ backend/imports.py
@@ -12,6 +12,9 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 load_dotenv() # load env variables from .env file
 
 #logger
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
 log_format = "[%(asctime)s] [%(levelname)s] %(message)s"
 logging.basicConfig(
     level=logging.WARNING,
@@ -26,7 +29,6 @@ logging.basicConfig(
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
-POSTER_BUCKET = "movie-posters"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_API_KEY)
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
@@ -38,39 +40,16 @@ genders = {
     2: 'male',
 }
 
+movie_batch = []
+movie_genres_batch = []
+crew_members_batch = []
+movie_actors_batch = []
+
 def fetch_movie_details(movie_id):
     time.sleep(random.randrange(10, 1000) / 1000)  # random sleep to avoid hitting API rate limits
     url = f"{TMDB_BASE_URL}/movie/{movie_id}?api_key={TMDB_API_KEY}&append_to_response=videos"
     res = requests.get(url)
     return res.json() if res.status_code == 200 else None
-
-def upload_poster(movie_id, poster_path):
-    if not poster_path:
-        return None
-    poster_url = f"https://image.tmdb.org/t/p/w500{poster_path}"
-    filename = f"{movie_id}.jpg"
-
-    while True:
-        try:
-            res = requests.get(poster_url)
-            if res.status_code != 200:
-                logging.error(f"⚠️ Failed to fetch poster {movie_id}, retrying in 0.1s...")
-                time.sleep(0.1)
-                continue
-
-            with NamedTemporaryFile(delete=False, suffix=".jpg") as tmp_file:
-                tmp_file.write(res.content)
-                tmp_file_path = tmp_file.name
-
-            supabase.storage.from_(POSTER_BUCKET).upload(filename, tmp_file_path, {
-                "content-type": "image/jpeg",
-                "x-upsert": "true"
-            })
-            os.remove(tmp_file_path)
-            return filename
-        except Exception as e:
-            logging.error(f"❌ Upload error for poster {movie_id}: {e}, retrying in 0.1s...")
-            time.sleep(0.1)
 
 def get_movie_certification(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}/release_dates"
@@ -90,34 +69,6 @@ def fetch_credits(movie_id):
     res = requests.get(url)
     return res.json() if res.status_code == 200 else None
 
-def upload_profile(member_id, profile_path):
-    if not profile_path:
-        return None
-    profile_url = f"https://image.tmdb.org/t/p/w500{profile_path}"
-    filename = f"{member_id}.jpg"
-
-    while True:
-        try:
-            res = requests.get(profile_url)
-            if res.status_code != 200:
-                logging.error(f"⚠️ Failed to fetch profile {member_id}, retrying...")
-                time.sleep(0.1)
-                continue
-
-            with NamedTemporaryFile(delete=False, suffix=".jpg") as tmp_file:
-                tmp_file.write(res.content)
-                tmp_file_path = tmp_file.name
-
-            supabase.storage.from_("crew-imgs").upload(filename, tmp_file_path, {
-                "content-type": "image/jpeg",
-                "x-upsert": "true"
-            })
-            os.remove(tmp_file_path)
-            return filename
-        except Exception as e:
-            logging.error(f"❌ Upload error for profile {member_id}: {e}, retrying...")
-            time.sleep(0.1)
-
 def fetch_person(person_id):
     url = f"{TMDB_BASE_URL}/person/{person_id}?api_key={TMDB_API_KEY}"
     res = requests.get(url)
@@ -127,18 +78,17 @@ def insert_movie_with_retry(movie_id):
     while True:
         try:
             movie = fetch_movie_details(movie_id)
-            # print(f"movie: {movie["id"]}")
+            print(f"movie: {movie["id"]}")
             if not movie:
-                logging.error(f"❌ Couldn't fetch details for {movie_id}")
+                logging.error(f"Couldn't fetch details for {movie_id}")
                 return
 
             credits = fetch_credits(movie_id)
             if not credits:
-                logging.error(f"❌ Couldn't fetch credits for {movie_id}")
+                logging.error(f"Couldn't fetch credits for {movie_id}")
                 return
-            # print(f"credits gotten")
-            poster_filename = upload_poster(movie["id"], movie["poster_path"])
-            # print(f"poster_filename: {poster_filename}")
+            print(f"credits gotten")
+
             trailer_key = None
             retry_count = 0
             max_retries = 3
@@ -148,12 +98,12 @@ def insert_movie_with_retry(movie_id):
                         trailer_key = video["key"]
                         break
                 if trailer_key is None:
-                    logging.error(f"⚠️ No trailer key for {movie_id}, retrying fetch ({retry_count+1})...")
+                    logging.error(f"No trailer key for {movie_id}, retrying fetch ({retry_count+1})...")
                     time.sleep(0.1)
                     movie = fetch_movie_details(movie_id)
                     retry_count += 1
 
-            # print(f"trailer_key: {trailer_key}")
+            print(f"trailer_key: {trailer_key}")
 
             rating = get_movie_certification(movie_id)
 
@@ -172,25 +122,30 @@ def insert_movie_with_retry(movie_id):
                 "popularity": movie.get("popularity"),
                 "lang_id": movie.get("original_language"),
                 "trailer_key": trailer_key,
-                "poster_path": poster_filename,
+                "poster_path": movie.get("poster_path"),
                 "rating": rating
             }
-            supabase.table("movie").upsert(movie_data).execute()
-            # print("after supabase insert")
+            if movie_data["release_date"] == "":
+                movie_data["release_date"] = None
+            if movie_data["lang_id"] == "cn":
+                movie_data["lang_id"] = "zh"
+            
+            movie_batch.append(movie_data)
+
+            print("after supabase insert")
             # insert genres
             for genre in movie.get("genres", []):
-                supabase.table("movie_genre").upsert({
+                movie_genres_batch.append({
                     "movie_id": movie["id"],
                     "genre_id": genre["id"]
-                }).execute()
-            # print("after genre insert")
+                })
+            print("after genre insert")
             # insert cast members
             for cast_member in credits.get("cast", [])[:20]:  # limit top 20 actors
                 actor_id = cast_member["id"]
                 if actor_id not in inserted_members:
                     person = fetch_person(actor_id)
                     if person:
-                        profile_filename = upload_profile(actor_id, person.get("profile_path"))
                         crew_data = {
                             "member_id": person["id"],
                             "name": person.get("name"),
@@ -198,20 +153,19 @@ def insert_movie_with_retry(movie_id):
                             "death_date": person.get("deathday"),
                             "birthday": person.get("birthday"),
                             "place_of_birth": person.get("place_of_birth"),
-                            "profile_url": profile_filename,
+                            "profile_url": person.get("profile_path"),
                             "popularity": person.get("popularity"),
                             "known_for": person.get("known_for_department"),
                             "gender": genders.get(person.get("gender", 0), "unknown")
                         }
-                        supabase.table("crew_member").upsert(crew_data).execute()
+                        crew_members_batch.append(crew_data)
                         inserted_members.add(actor_id)
                 # insert into movie-actor
-                supabase.table("movie_actor").upsert({
+                movie_actors_batch.append({
                     "movie_id": movie["id"],
                     "actor_id": actor_id
-                }).execute()
-            # print("after cast insert")
-
+                })
+            print("after cast insert")
             # insert director
             for crew_member in credits.get("crew", []):
                 if crew_member.get("job") == "Director":
@@ -219,7 +173,6 @@ def insert_movie_with_retry(movie_id):
                     if director_id not in inserted_members:
                         person = fetch_person(director_id)
                         if person:
-                            profile_filename = upload_profile(director_id, person.get("profile_path"))
                             crew_data = {
                                 "member_id": person["id"],
                                 "name": person.get("name"),
@@ -227,31 +180,49 @@ def insert_movie_with_retry(movie_id):
                                 "death_date": person.get("deathday"),
                                 "birthday": person.get("birthday"),
                                 "place_of_birth": person.get("place_of_birth"),
-                                "profile_url": profile_filename,
+                                "profile_url": person.get("profile_path"),
                                 "popularity": person.get("popularity"),
                                 "known_for": person.get("known_for_department"),
                                 "gender": genders.get(person.get("gender", 0), "unknown")
                             }
-                            supabase.table("crew_member").upsert(crew_data).execute()
+                            crew_members_batch.append(crew_data)
                             inserted_members.add(director_id)
 
                     # insert into movie-actor
-                    supabase.table("movie_actor").upsert({
+                    movie_actors_batch.append({
                         "movie_id": movie["id"],
                         "actor_id": director_id
-                    }).execute()
+                    })
                     break
-            # print("after director insert")
+            print("after director insert")
             break  # finished successfully
         except Exception as e:
-            logging.error(f"⚠️ Network error for movie {movie_id}, retrying in 0.1s... ({str(e)})")
+            logging.error(f"Network error movie {movie_id}, retrying... ({str(e)})")
             time.sleep(0.1)
 
+def flush_batches():
+    global movie_batch, movie_genres_batch, crew_members_batch, movie_actors_batch
+    try:
+        if movie_batch:
+            supabase.table("movie").upsert(movie_batch).execute()
+        if movie_genres_batch:
+            supabase.table("movie_genre").upsert(movie_genres_batch).execute()
+        if crew_members_batch:
+            supabase.table("crew_member").upsert(crew_members_batch).execute()
+        if movie_actors_batch:
+            supabase.table("movie_actor").upsert(movie_actors_batch).execute()
+    except Exception as e:
+        logging.error(f"Error flushing batches: {str(e)}")
+    finally:
+        movie_batch.clear()
+        movie_genres_batch.clear()
+        crew_members_batch.clear()
+        movie_actors_batch.clear()
 
 def main():
     with open("progress.txt") as f:
         page = int(f.read())
-    max_pages = page + 30
+    max_pages = page + 5
     max_workers = 5  # number of threads to use for concurrent requests
     start_time = time.time()
     while page <= max_pages:
@@ -259,21 +230,25 @@ def main():
         try:
             res = requests.get(f"{TMDB_BASE_URL}/movie/popular?api_key={TMDB_API_KEY}&page={page}")
             if res.status_code != 200:
-                logging.error(f"❌ Failed to fetch page {page}")
+                logging.error(f"Failed to fetch page {page}")
                 break
             results = res.json().get("results", [])
 
             with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                print("here")
                 futures = [executor.submit(insert_movie_with_retry, movie["id"]) for movie in results]
                 for future in as_completed(futures):
                     future.result()
+
+            
+            flush_batches()
 
             page += 1
             with open("progress.txt", "w") as f:
                 f.write(str(page))
             logging.warning(f"page: {page} in progress")
         except Exception as e:
-            logging.warning(f"⚠️ Error fetching page {page}, retrying in 0.1s... ({e})")
+            logging.warning(f"Error fetching page {page}, retrying... ({e})")
             time.sleep(0.1)
 
 if __name__ == "__main__":
